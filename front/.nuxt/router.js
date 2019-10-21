a65d8681a2605cb9b11d3a8011a110482a185ef3
KAWASAKI <yuko124529@yahoo.co.jp>
2019-10-21 20:18:43 +0900

    about page

--- front/.nuxt/router.js
+++ front/.nuxt/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _0e433b93 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
 const _34780b0c = () => interopDefault(import('../pages/signin.vue' /* webpackChunkName: "pages/signin" */))
 const _2dfb1658 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
@@ -16,6 +17,10 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/about",
+    component: _0e433b93,
+    name: "about"
+  }, {
     path: "/signin",
     component: _34780b0c,
     name: "signin"
---
+++ front/pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar is-white" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <div class="navbar-item">Avocado</div>
+      <div class="navbar-burger" data-target="navMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div><!-- navbar-brand END -->
+    <div class="navbar-menu" id="navMenu">
+      <div class="navbar-end">
+        <nuxt-link to="/" class="navbar-item">トップ</nuxt-link>
+        <nuxt-link to="/about" class="navbar-item">Avocadoとは？</nuxt-link>
+        <nuxt-link to="/contact" class="navbar-item">お問い合わせ</nuxt-link>
+      </div>
+    </div><!-- navbar-menu END -->
+  </nav>
+</template>
