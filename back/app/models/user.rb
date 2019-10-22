dfacf27395d7c6a250466bd5e3bb3d53338d0f0e
KAWASAKI <yuko124529@yahoo.co.jp>
2019-10-22 18:21:45 +0900

    devise-token-auth やり直し

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,2 +1,9 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
 end
