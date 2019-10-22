5ea454ac1d34497f48357e956bb797db02f042f8
KAWASAKI <yuko124529@yahoo.co.jp>
2019-10-23 00:19:43 +0900

    auth strong_params

--- back/app/controllers/auth/registrations_controller.rb
+++ back/app/controllers/auth/registrations_controller.rb
@@ -1,2 +1,9 @@
 class Auth::RegistrationsController < ApplicationController
-end
+  private
+    def sign_up_params
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
+    def account_update_params
+      params.permit(:name, :email)
+    end
+  end
