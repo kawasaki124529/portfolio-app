c5015aeb4813e5fab397eec26dd112ff0b2d812e
KAWASAKI <yuko124529@yahoo.co.jp>
2019-10-22 19:22:48 +0900

    routes 修正

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'  controllers: {
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
