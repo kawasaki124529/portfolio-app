1c11421a7504a3217bb441607f368bc8f2a31ab5
KAWASAKI <yuko124529@yahoo.co.jp>
2019-10-23 01:09:18 +0900

    modify controller

--- back/app/controllers/application_controller.rb
+++ back/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::API
+        skip_before_action :verify_authenticity_token, if: :devise_controller?, raise: false  # APIではCSRFチェックをしない
         include DeviseTokenAuth::Concerns::SetUserByToken
-        skip_before_action :verify_authenticity_token, if: :devise_controller? # APIではCSRFチェックをしない
 end
