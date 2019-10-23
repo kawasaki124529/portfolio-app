d10e17e82701924bf775f4ac1a819fc21e72ed61
KAWASAKI <yuko124529@yahoo.co.jp>
2019-10-23 16:02:07 +0900

    signUp form #1

--- front/.nuxt/router.js
+++ front/.nuxt/router.js
@@ -4,7 +4,9 @@ import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
 const _1bbad8e0 = () => interopDefault(import('../pages/inspire.vue' /* webpackChunkName: "pages/inspire" */))
-const _10ba8d22 = () => interopDefault(import('../pages/login.vue' /* webpackChunkName: "pages/login" */))
+const _490e6182 = () => interopDefault(import('../pages/signup.vue' /* webpackChunkName: "pages/signup" */))
+const _4e550fc0 = () => interopDefault(import('../pages/signup2.vue' /* webpackChunkName: "pages/signup2" */))
+const _7f799735 = () => interopDefault(import('../pages/user.vue' /* webpackChunkName: "pages/user" */))
 const _2dfb1658 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 Vue.use(Router)
@@ -21,9 +23,17 @@ export const routerOptions = {
     component: _1bbad8e0,
     name: "inspire"
   }, {
-    path: "/login",
-    component: _10ba8d22,
-    name: "login"
+    path: "/signup",
+    component: _490e6182,
+    name: "signup"
+  }, {
+    path: "/signup2",
+    component: _4e550fc0,
+    name: "signup2"
+  }, {
+    path: "/user",
+    component: _7f799735,
+    name: "user"
   }, {
     path: "/",
     component: _2dfb1658,
---
+++ front/pages/signup2.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            md="10"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>SignUp form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Email"
+                    name="email"
+                    prepend-icon=""
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon=""
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">SIGNUP</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    // props: {
+    //   source: String,
+    // },
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
