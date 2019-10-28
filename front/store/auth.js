0f8adb945e5c2ca04f14fa2f2ac6b2157b8b0da0
KAWASAKI <yuko124529@yahoo.co.jp>
2019-10-28 21:01:13 +0900

    認証　store create

---
+++ front/store/auth.js
@@ -0,0 +1,48 @@
+export const state = () => {
+  return {
+    Tokens : {}, // accessToken, uid, expiry wo kakunou
+    // accessToken: null, 
+    // uid: null,
+    // expiry: null,
+  }
+}
+
+export const mutations = {
+  updateTokens(state, Tokens){
+    state.Tokens = Tokens;
+  },
+  updateAccessToken(state, accessToken){
+    state.accessToken = accessToken;
+    console.log(state.accessToken)
+  },
+  updateUid(state, uid){
+    state.uid = uid;
+    console.log(state.uid)
+  },
+  updateAccessToken(state, expiry){
+    state.accessToken = expiry;
+    console.log(state.expiry)
+  },
+}
+
+export const actions = {
+  login({ commit }, authData) {
+    this.$axios
+          .post(
+            '/auth/sign_in',
+            {
+              email: authData.email,
+              password: authData.password,
+            }
+          )
+          .then(response => {
+            commit('updateTokens', {
+              accessToken: response.headers['access-token'],
+              expiry: response.headers.expiry,
+              uid: response.headers.uid
+            });
+            // commit('updateUid', response.headers.uid)
+            console.log(response)
+          });
+  } 
+}
