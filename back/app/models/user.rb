0897b5f2234124224d3d9c6e8258f13f39acb58f
KAWASAKI <yuko124529@yahoo.co.jp>
2019-11-18 19:37:54 +0900

    user model 変更

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :topics, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
