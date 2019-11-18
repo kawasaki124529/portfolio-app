59216fb9adbb6a97f56a4cda00e68c0059911201
KAWASAKI <yuko124529@yahoo.co.jp>
2019-11-18 19:38:20 +0900

    rails routes 変更

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
+  # namespaceを利用することで、 /api/each_api 形式のrouting設定を行う
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       registrations: 'api/auth/registrations'
     }
+    resources :topics, only: %i(new create)
   end  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
