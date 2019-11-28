e2e7f8cfd1c4009fe5ed76ada588d7598c077c0f
KAWASAKI <yuko124529@yahoo.co.jp>
2019-11-28 15:56:10 +0900

    topic db 修正

--- back/db/migrate/20191118054936_create_topics.rb
+++ back/db/migrate/20191118054936_create_topics.rb
@@ -1,11 +1,12 @@
 class CreateTopics < ActiveRecord::Migration[5.2]
   def change
     create_table :topics do |t|
+      t.string :shop_name, null: false
       t.string :date, null: false
       t.string :meals, null: false
       t.string :price, null: false
       t.string :review, null: false 
-      t.string :rating, null: false
+      t.integer :rating, null: false
       t.string :image, null: false
       t.references :user, foreign_key: true, null: false
       t.timestamps
