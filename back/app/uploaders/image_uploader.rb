0db3c7982e3c78d0f7144c20b7d2f7315f3851a6
KAWASAKI <yuko124529@yahoo.co.jp>
2019-11-28 15:55:19 +0900

    image uploader 修正

--- back/app/uploaders/image_uploader.rb
+++ back/app/uploaders/image_uploader.rb
@@ -13,6 +13,16 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # 許可する画像の拡張子
+  def extension_white_list
+    %W[jpg jpeg gif png]
+  end
+
+  # 変換したファイルのファイル名の規則
+  def filename
+    "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename.present?
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
