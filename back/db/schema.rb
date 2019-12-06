736efaabd1c76a9a960613bd137b9979e6301a03
KAWASAKI <yuko124529@yahoo.co.jp>
2019-12-06 15:45:30 +0900

    確認不要

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_054936) do
+ActiveRecord::Schema.define(version: 2019_12_05_122356) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "content"
+  end
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "shop_name", null: false
@@ -44,7 +59,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_054936) do
     t.string "image"
     t.string "email"
     t.string "team"
-    t.text "tokens"
+    t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
