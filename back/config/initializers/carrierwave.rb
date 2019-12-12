f6f260410b3c345b48b0e7b4211e58e7cc256844
KAWASAKI <yuko124529@yahoo.co.jp>
2019-12-12 20:01:40 +0900

    AWS S3 configs

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,3 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
-  config.asset_host = 'http://localhost:8000'
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'rails-photos'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1',
+      path_style: true
+    }
+  else
+    config.asset_host = 'http://localhost:8000'
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
