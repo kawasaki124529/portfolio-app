fccb10c2a850cabe15a2788919e7b042f1fa342c
KAWASAKI <yuko124529@yahoo.co.jp>
2019-12-17 16:12:42 +0900

    ソートセレクタ　導入

--- front/pages/topics.vue
+++ front/pages/topics.vue
@@ -3,153 +3,15 @@
     <v-flex xs12 sm12 md12>
       <!-- グレー背景 -->
       <v-card class="grey lighten-3 mt-4 mb-4 pa-3 ">
-        <v-container fluid grid-list-md>
-          <v-item-group>
-            <v-layout row wrap>
-              <v-flex v-for="(topic, i) in topics" :key="i" xs12 sm6 md4>
-                <!-- トピック個別 -->
-                <v-item v-slot:default="{ active, toggle }">
-                  <v-hover>
-                    <v-card
-                      slot-scope="{ hover }"
-                      class="brown lighten-4 ma-2"
-                      :class="`elevation-${hover ? 12 : 2}`"
-                      @click="toggle"
-                    >
-                      <v-card-text>
-                        <v-img
-                          :aspect-ratio="16 / 9"
-                          :src="topic.image.url"
-                          width="100%"
-                        >
-                          <template v-slot:placeholder>
-                            <v-layout
-                              fill-height
-                              align-center
-                              justify-center
-                              ma-0
-                            >
-                              <v-progress-circular
-                                indeterminate
-                                color="grey lighten-5"
-                              />
-                            </v-layout>
-                          </template>
-                        </v-img>
-                        <!-- 画像下テキスト欄 -->
-                        <v-card-title>
-                          <div>
-                            <span
-                              class="subtitle-2 font-weight-medium font-italic"
-                              >{{ topic.shop_name }}</span
-                            >
-                            <div class="d-flex">
-                              <v-rating
-                                :value="topic.rating"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="14"
-                              />
-                              <div class="ml-2 grey--text text--darken-2">
-                                <span>{{ topic.rating }}</span>
-                                <span class="caption">(おすすめ度)</span>
-                              </div>
-                            </div>
-                          </div>
-                          <v-spacer />
-                          <v-icon small class="mr-0" color="red">
-                            far fa-heart
-                          </v-icon>
-                          <span class="ma-1 grey--text text--darken-1">{{
-                            topic.likes.length
-                          }}</span>
-                          <v-icon small class="mr-0" color="grey">
-                            far fa-comment
-                          </v-icon>
-                          <span class="ma-1 grey--text text--darken-1">{{
-                            topic.comments.length
-                          }}</span>
-                        </v-card-title>
-                      </v-card-text>
-                      <!-- モーダル部分 -->
-                      <v-dialog v-model="active" max-width="800px">
-                        <v-card class="brown lighten-4">
-                          <v-card-title>
-                            <span class="brown--text text--darken-1">{{
-                              topic.user.name
-                            }}</span>
-                            <span class="body-1 mr-2">さんの投稿</span>
-                            <span class="caption brown--text text--darken-1"
-                              >({{ topic.date }}に訪問)</span
-                            >
-                            <v-spacer />
-                            <v-btn icon @click="toggle">
-                              <v-icon>fas fa-times</v-icon>
-                            </v-btn>
-                            <v-img
-                              :aspect-ratio="16 / 9"
-                              :src="topic.image.url"
-                              width="100%"
-                            />
-                          </v-card-title>
-                          <v-card-text>
-                            <div class="d-inline-flex">
-                              <span
-                                class="title brown--text text--darken-1 mr-2"
-                              >{{ topic.shop_name }}</span>
-                              <span
-                                class="subheading brown--text text--darken-1 pt-2 mr-2"
-                              >・{{ topic.meals }}</span>
-                              <span
-                                class="subheading brown--text text--darken-2 pt-2"
-                              >予算：{{ topic.price }}</span>
-                              <v-rating
-                                class="ml-1 mt-1"
-                                :value="topic.rating"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="14"
-                              />
-                              <span class="mt-2 mx-1">{{ topic.rating }}</span>
-                              <span class="caption mt-2">(おすすめ度)</span>
-                            </div>
-                            <v-divider />
-                            <div>
-                              <br />
-                              <span>{{ topic.review }}</span>
-                            </div>
-                          </v-card-text>
-                          <v-card-actions>
-                            <v-spacer />
-                            <!-- いいねコンポーネント -->
-                            <LikeBtn :topic="topic" />
-                            <!-- コメントコンポーネント -->
-                            <v-btn
-                              outlined
-                              color="grey darken-1"
-                              @click="comments = !comments"
-                            >
-                              <v-icon>far fa-comment-dots</v-icon>
-                            </v-btn>
-                          </v-card-actions>
-                          <v-slide-y-transition>
-                            <v-card-actions v-show="comments">
-                              <CommentArea :topic="topic" />
-                            </v-card-actions>
-                          </v-slide-y-transition>
-                        </v-card>
-                      </v-dialog>
-                    </v-card>
-                  </v-hover>
-                </v-item>
-              </v-flex>
-            </v-layout>
-          </v-item-group>
+        <v-container fluid>
+          <v-radio-group v-model="sortKey" row>
+            <v-radio label="投稿が新しい順" value="date"></v-radio>
+            <v-radio label="いいね数" value="likes"></v-radio>
+            <v-radio label="おすすめ度" value="rating"></v-radio>
+          </v-radio-group>
         </v-container>
+        <!-- トピックコンポーネントにソートされたデータを渡す -->
+        <AllTopics :topics="TopicOrderBy"></AllTopics>
       </v-card>
       <!-- トピック投稿コンポーネント -->
       <NewTopicBtn v-show="isLoggedIn" />
@@ -159,14 +21,12 @@
 
 <script>
 import NewTopicBtn from "../components/NewTopicBtn.vue"
-import LikeBtn from "../components/LikeBtn.vue"
-import CommentArea from "../components/CommentArea.vue"
+import AllTopics from "../components/AllTopics.vue"
 
 export default {
   components: {
     NewTopicBtn,
-    LikeBtn,
-    CommentArea
+    AllTopics
   },
   // ページ読み込み時にTopicコントローラーにgetリクエスト
   async asyncData({ app }) {
@@ -176,13 +36,26 @@ export default {
   },
   data() {
     return {
-      comments: false
+      comments: false,
+      sortKey: "date"
     }
   },
   computed: {
     isLoggedIn() {
       return this.$store.state.auth.isLoggedIn
-    }
+    },
+    // セレクタの値によってソートの切り替え
+    TopicOrderBy() {
+      if (this.sortKey === "date" ) {
+        return _.orderBy(this.topics, ['date'], ['asc']);
+      }
+      else if ( this.sortKey === "likes" ) {
+        return _.orderBy(this.topics, [`likes`], ['desc']);
+      }
+      else if ( this.sortKey === "rating" ) {
+        return _.orderBy(this.topics, [`rating`], ['desc']);
+      }
+    },
   }
 }
 </script>
